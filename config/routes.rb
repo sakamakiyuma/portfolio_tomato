0e327c41f0059802fd570867fce0cc4b9949df5e
sakamakiyuma <yuma78733@yahoo.co.jp>
2019-09-19 16:14:07 +0900

    gallery投稿機能１
    carrier wave実装後、indexにてcssが効かなくなってしまった。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'gallery/index'
+  get "gallery" => "gallery#index"
+  get "gallery/new" => "gallery#new"
+  # get "gallery/:id"  => "gallery#show"
+  post "gallery/create" => "gallery#create"
+  
+  
+  # post '/gallery/create', to: 'gallery#create'
+
   get "/" => "home#top"
   get "about" => "home#about"
 end
