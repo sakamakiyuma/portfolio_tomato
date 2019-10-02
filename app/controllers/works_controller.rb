ad526f2b8cde3c7af34e4ae07f0a9da4a1d018ce
sakamakiyuma <yuma78733@yahoo.co.jp>
2019-10-02 23:02:42 +0900

    works2
    投稿機能実装
    
    次回、削除機能

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,4 +1,20 @@
 class WorksController < ApplicationController
+  before_action :authenticate_user,{only:[:new,:create]}
+
   def index
+    @works = Work.all.order(created_at: :desc)
+  end
+
+  def new
+  end
+
+  def create
+    @works = Work.new(permit_params)
+    @works.save!
+  end
+  
+private
+  def permit_params
+    params.require(:work).permit(:image)
   end
 end
